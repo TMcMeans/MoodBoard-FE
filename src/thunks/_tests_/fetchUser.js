7db6ab6f72cbb776ffd152c42671e6ef5e4cbfc4
Tanjie McMeans <tanjmcmeans01@gmail.com>
2019-02-17 13:49:52 -0700

    add fetchUser thunk

---
+++ src/thunks/_tests_/fetchUser.js
@@ -0,0 +1,11 @@
+import { hasErrored, loginUser } from '../actions';
+
+export const fetchUser = (url, user) => {
+  return async dispatch => {
+    try {
+      //Write async logic once endpoint has been solidified on BE
+    } catch (error) {
+      dispatch(hasErrored(error.message));
+    }
+  };
+};
