7de117fba589ae39c25cb949350bab221a7efbb6
Tanjie McMeans <tanjmcmeans01@gmail.com>
2019-02-19 12:47:07 -0700

    add hardcoded data for inspirationalquotes and prompts

---
+++ src/helper/inpirationalQuotes.js
@@ -0,0 +1,65 @@
+export const inspirationalQuotes = {
+  joy: [
+    {
+      author: 'Thich Nhat Hanh',
+      quote:
+        '“Sometimes your joy is the source of your smile, but sometimes your smile can be the source of your joy.”'
+    },
+    {
+      author: 'Rumi',
+      quote:
+        '“When you do things from your soul, you feel a river moving in you, a joy.”'
+    },
+    {
+      author: 'Tony DeLiso',
+      quote: '“Happiness is part of who we are. Joy is the feeling”'
+    },
+    {
+      author: 'Aberjhani',
+      quote:
+        '“Dare to love yourself as if you were a rainbow with gold at both ends.”'
+    }
+  ],
+  confident: [
+    {
+      author: 'Lailah Gifty Akita',
+      quote: '“There is nothing like boldness of being.”'
+    },
+    {
+      author: 'Jay Woodman',
+      quote: '“Be confident to make your magic.”'
+    },
+    {
+      author: 'Lillian Cauldwell',
+      quote:
+        "“Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.”"
+    },
+    {
+      author: 'Criss Jami',
+      quote:
+        '“Confidence is like a dragon where, for every head cut off, two more heads grow back.”'
+    }
+  ],
+  analytical: [
+    {
+      author: 'Bryant McGill',
+      quote:
+        '“Carefully analyze the old stories that you choose to perpetually tell, for these are really life-long meditations.”'
+    },
+    {
+      author: 'Christopher Hitchens',
+      quote:
+        '“The essence of the independent mind lies not in what it thinks, but in how it thinks.”'
+    },
+    {
+      author: 'Stefan Molyneux',
+      quote:
+        '“Truth has nothing to do with the conclusion, and everything to do with the methodology.”'
+    },
+    {
+      author: 'Richard Paul',
+      quote:
+        "“Critical thinking is thinking about your thinking while you're thinking in order to make your thinking better.” "
+    }
+  ]
+};
---
+++ src/helper/prompts.js
@@ -0,0 +1,52 @@
+export const prompts = {
+  fear: [
+    'My courage is stronger than my fear.',
+    'Although I am afraid, I can learn to be confident in myself and my abilities.',
+    'There is more to me than this battle.',
+    'I can learn to accept and embrace fear as a sign that I am being faced with an opportunity to grow.',
+    'I am the artist and creator of my life story.',
+    'I forgive myself for not being perfect.',
+    'I have the courage to say, no.',
+    'I am prioritizing my physical, mental health and self care today.',
+    'I deserve love, compassion and empathy.',
+    'I accept myself unconditionally.',
+    'This fear is only temporary.'
+  ],
+  tentative: [
+    'To be courageous is to begin a love affair with the unknown. Embrace it.',
+    "You can't be hesitant about who you are. Be yourself and nothing else.",
+    'I can surround myself with people who see my value and remind me of it.',
+    'I am allowed to take space, be hesitant, be bold, ask questions and exist on this earth.',
+    'Confidence is something I create within myself by believing in who I am.',
+    'I forgive myself for not being perfect.',
+    'I have the courage to say, no.',
+    'I am prioritizing my physical, mental health and self care today.',
+    'I believe in the person I dream of becoming.',
+    'I accept myself unconditionally.'
+  ],
+  sadness: [
+    'I alone am enough. I have nothing to prove to anyone.',
+    "It's okay to be sad and cry sometimes.",
+    'This too shall pass. I can withstand this.',
+    "I've survived before, and I will survive now.",
+    'I forgive myself for not being perfect.',
+    'I was not made to give up.',
+    'I am prioritizing my physical, mental health and self care today.',
+    'I deserve love, compassion and empathy.',
+    'I accept myself unconditionally.',
+    'This sadness is only temporary.'
+  ],
+  anger: [
+    "I will not say anything about myself that I don't want to come true.",
+    'Feelings are just visitors. Let them come and go.',
+    "It's okay to be mad at something or someone that hurts you.",
+    'I have the power to stand up for myself.',
+    'I forgive myself for not being perfect.',
+    "Don't ever mistake my silence for ignorance, my calmness for acceptance and my kindness for weakness.",
+    'I have the courage to say, no.',
+    'I am prioritizing my physical, mental health and self care today.',
+    'I deserve love, compassion and empathy.',
+    'I accept myself unconditionally.',
+    'This '
+  ]
+};
