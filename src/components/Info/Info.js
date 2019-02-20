919259f878e5b023ac4a386c9036ab0cccf30ff4
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-02-20 15:29:09 -0700

    Add styling to info component and add info paragraph.

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { Switch, Route, withRouter } from 'react-router-dom';
 
+import './Info.css';
+
 class Info extends Component {
   render(){
     return(
       <div>
-        <h1> Welcome to MoodBoard. </h1>
+        <h1 className ="info-heading"> Welcome to MoodBoard. </h1>
+        <p className = "info-paragraph"> MoodBoard is a daily journaling tool, record of personal reflections, and long-term reference for a user’s projected mood on any given day. </p>
       </div>
 
     );
