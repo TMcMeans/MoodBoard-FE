4ee63b81edeeb0a8da11086649dafed8b3906526
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-02-20 15:32:08 -0700

    Export logo to info page.

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import { Switch, Route, withRouter } from 'react-router-dom';
 
 import './Info.css';
+import Logo from '../../components/Logo/Logo';
 
 class Info extends Component {
   render(){
     return(
       <div>
-        <h1 className ="info-heading"> Welcome to MoodBoard. </h1>
+      <Logo />
+
+        <h1 className ="info-heading"> About </h1>
         <p className = "info-paragraph"> MoodBoard is a daily journaling tool, record of personal reflections, and long-term reference for a user’s projected mood on any given day. </p>
       </div>
 
