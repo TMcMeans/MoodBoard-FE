8e50a0cbd979aa46ebaa7a3a3ff6d9273d69b059
Tanjie McMeans <tanjmcmeans01@gmail.com>
2019-02-19 17:52:43 -0700

    add thunk for getJournalEntryByDate and passing tests

---
+++ src/thunks/getJournalEntryByDate.js
@@ -0,0 +1,16 @@
+import { hasErrored, saveJournalEntry } from '../actions';
+
+export const getJournalEntryByDate = url => {
+  return async dispatch => {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      const journal_entry = await response.json();
+      await dispatch(saveJournalEntry(journal_entry.data.attributes));
+    } catch (error) {
+      dispatch(hasErrored(error.message));
+    }
+  };
+};
