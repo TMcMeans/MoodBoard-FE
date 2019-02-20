4a0222667a8bf908c068c7c561d3abd318dc9e05
Ashton Bailey <ashtonkbailey@gmail.com>
2019-02-19 18:55:18 -0700

    Add action, reducer, and thunk to get all month tonesin CalendarView

--- src/containers/CalendarView/CalendarView.js
+++ src/containers/CalendarView/CalendarView.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import Calendar from 'react-calendar/dist/entry.nostyle';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import Logo from '../../components/Logo/Logo';
 import Button from '../../components/Button/Button';
+import { getTonesByMonth } from '../../thunks/getTonesByMonth';
 import './CalendarView.css';
 
 class CalendarView extends Component {
@@ -18,6 +20,14 @@ class CalendarView extends Component {
     this.setState({ date });
   };
 
+  componentDidMount = async () => {
+    // get the date (mo, yr)
+    console.log(this.state.date)
+    // get the tones
+    // set the tones to colors
+    // assign colors to individual days (by the day's value)
+  }
+
   render() {
     return (
       <div className="calendar">
@@ -31,4 +41,10 @@ class CalendarView extends Component {
   }
 }
 
-export default CalendarView;
+const mapDispatchToProps = dispatch => ({
+  getTonesByMonth: (url) => dispatch(getTonesByMonth(url))
+})
+
+export default connect(null, mapDispatchToProps)(CalendarView);
+
+///api/v1/users/1/tone_responses?date=YYYY-MM
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import { journalReducer } from './journalReducer';
 import { errorReducer } from './errorReducer';
 import { loadingReducer } from './loadingReducer';
 import { userReducer } from './userReducer';
+import { tonesReducer } from './tonesReducer';
 
 export const rootReducer = combineReducers({
   journal: journalReducer,
   user: userReducer,
   error: errorReducer,
-  isLoading: loadingReducer
+  isLoading: loadingReducer,
+  tones: tonesReducer
 });
